aedaf58eafc2c822a3eba90ebdfb1074f9900627
GaloAlmiron <82978178+GaloAlmiron@users.noreply.github.com>
2022-04-27 15:41:28 -0300

    Delete KataOne/calculator directory

--- KataOne/calculator/calculator_POO.cpp/build.bat
+++
@@ -1,9 +0,0 @@
-:: Compilo c+odigo objeto
-g++ -Wall -std=c++11 -c .\src\calculator.cpp 
-g++ -Wall -std=c++11 -c main.cpp 
-
-:: Compilo el Binario
-g++ calculator.o main.o -o calculator.exe
-
-:: Limpio los códigos objeto
-DEL .\*.o
